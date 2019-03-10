39958cf2136a49c60ed963a4a62654a19d25e30a
Sibsankar Bera <sibsankarb4@gmail.com>
2019-03-10 19:18:07 +0530

    Add files via upload

---
+++ spring-cloud-aws/src/main/java/com/sibsankar/spring/cloud/aws/ec2/EC2EnableMetadata.java
@@ -0,0 +1,9 @@
+package com.sibsankar.spring.cloud.aws.ec2;
+
+import org.springframework.cloud.aws.context.config.annotation.EnableContextInstanceData;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableContextInstanceData
+public class EC2EnableMetadata {
+}
